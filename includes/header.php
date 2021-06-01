94898f1e81b8176899354c3ba636037ed6c6c858
s4gor <imrans4gor@gmail.com>
2021-06-02 01:00:29 +0600

    initial push

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once './config/config.php';
+
+if(!isset($title)) {
+	$title = 'Set a title';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?= $title ?></title>
+	<script type="text/javascript" src="<?= ASSET . '/scripts/main.js'; ?>"></script>
+    <link rel="stylesheet" href="<?= ASSET . '/styles/main.css'; ?>">
+    <?php
+    if(isset($jsLinker)) {
+    foreach ($jsLinker as $link) {
+        $js = '<script type="text/javascript" src="' . ASSET . '/scripts/' . $link . '"></script>';
+        echo $js;
+        }
+    }
+    if(isset($cssLinker)) {
+        foreach ($cssLinker as $link) {
+            $css = '<link rel="stylesheet" href="' . ASSET . '/styles/' . $link . '">';
+            echo $css;
+        }
+    }
+    ?>
+</head>
+<body>
---
+++ template.php
@@ -0,0 +1,24 @@
+<?php
+
+// title of page
+
+$title = 'Home';
+
+//linking css
+
+$cssLinker = [];
+
+//linking js
+
+$jsLinker = [];
+
+require_once './includes/header.php';
+
+?>
+
+
+
+
+<?php
+
+require_once './includes/footer.php';
